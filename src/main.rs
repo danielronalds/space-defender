c56320a5d8601104b46115535e95fc4cafb39354
Daniel R <nzdanron@gmail.com>
2024-02-13 18:32:19 +1300

    feat: Rendering SDL window
    Got a basic window going

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use sdl2::{image::{self, InitFlag}, event::Event, keyboard::Keycode};
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+
+    let video_subsystem = sdl_context.video()?;
+
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG);
+
+    let window = video_subsystem
+        .window("Space Defender", 1920, 1080)
+        .position_centered()
+        .build()
+        .expect("Failed to build window");
+
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .expect("Failed to build canvas");
+
+    let mut event_pump = sdl_context.event_pump()?;
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), ..} => {
+                    break 'running;
+                },
+                _ => (),
+            }
+        }
+        canvas.present();
+    }
+
+    Ok(())
 }
